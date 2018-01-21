16d76d0e40ca677e63076fa7e12a5c354950a69e
alezeero2 <alezeero2@gmail.com>
2018-01-21 20:15:31 +0100

    Aggiunta di un progetto WPF e di una Library

---
+++ Solution1/ConsoleApp1/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string scuola = "";
+            string programma = "";
+
+            Console.WriteLine("Inserisci il nome della scuola");
+            scuola = Console.ReadLine();
+            Console.WriteLine("Inserisci il nome del programma");
+            programma = Console.ReadLine();
+
+            Console.ReadLine();
+        }
+    }
+}
